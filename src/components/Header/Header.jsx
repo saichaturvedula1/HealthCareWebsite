6914e5acd9d7eb34e88166aee228e554bedda3dd
saichaturvedula1 <saichaturvedula@gmail.com>
2024-04-14 17:07:34 -0400

    header file changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,17 @@ import logoImage from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
-  const headerText = "Jcare Home Health Agency";
+  const headerText = "Home Health Services";
   const phoneNumber = "972-264-2737";
+  const faxNumber = "972-666-0507";
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        {/* <Link className="navbar-brand" to="/"> */}
-          <img src={logoImage} alt="Avatar Healthcare Logo" style={{ height: '80px'}} />
-        {/* </Link> */}
+        <img src={logoImage} alt="Avatar Healthcare Logo" style={{ height: '80px'}} />
         <div className="headerText">{headerText}</div>
         <div className="navbar-text">
-            Contact A Nurse :<a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
+            <span>Contact :<a href={`tel:${phoneNumber}`}>{phoneNumber}</a></span>
+            <span>Fax :<a href={`tel:${faxNumber}`}>{faxNumber}</a></span>
           </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
