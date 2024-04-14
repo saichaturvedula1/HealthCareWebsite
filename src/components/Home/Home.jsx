b96f0eee220fdb2f73b54858ef07245f4f316fb7
saichaturvedula1 <saichaturvedula@gmail.com>
2024-04-14 14:56:29 -0400

    unwanted imports in home.jsx removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Home.css';
 import { Fade, Slide} from 'react-awesome-reveal';
 import { useNavigate } from 'react-router-dom';
